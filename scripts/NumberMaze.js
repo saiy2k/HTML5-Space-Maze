5d815d84704cc7c476620602f4dcddd7d4ee7447
Saiyasodharan <saiy2k@gmail.com>
2012-04-17 17:13:43 +0530

    playtomic simple score save n retrival logic implemented

--- scripts/NumberMaze.js
+++ scripts/NumberMaze.js
@@ -202,14 +202,6 @@ along with Number Maze.  If not, see <http://www.gnu.org/licenses/>.
                 self.creditsScreen.draw(self.screenCtx);
             }
             window.requestAnimFrame(gameLoop);
-
-            Playtomic.Log.View(7158, "b34119c5c7074dd4", "883aa0c303e544fe9900683df59b0f", document.location);
-
-            var simple_score = {};
-            simple_score.Name = 'sai';;
-            simple_score.Points = 100;;
-
-            Playtomic.Leaderboards.Save(simple_score, "easy"); 
 /*
             Playtomic.GeoIP.Lookup(callback);
 
@@ -222,5 +214,15 @@ if(response.Success)
 */
 
         })();
-    };
+ 
+            console.log('updating score');
+            Playtomic.Log.View('7158', "b34119c5c7074dd4", "883aa0c303e544fe9900683df59b0f", document.location);
+
+            var simple_score = {};
+            simple_score.Name = 'bai';
+            simple_score.Points = 20;
+
+            Playtomic.Leaderboards.Save(simple_score, "easy"); 
+
+   };
 })(jQuery);
